7f9a0c1fe09321c1f7f9533977fca061ad33e7a8
Mohit Jain <jain.mohit27@gmail.com>
2020-07-19 16:42:50 +0530

    Added Basic Api put request

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
 
 class HelloApiView(APIView):
     """Test Api View"""
+    serializers_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """Returns a list of API View features"""
@@ -14,3 +18,14 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': "Hello", 'as_apiview': an_apiview})
+
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializers_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
